d4eb1079affeafea0df20f5fae9d72bfd36262d4
corriol <vjorda@gmail.com>
2024-01-08 20:01:15 +0100

    canvi de AssertMapper a Webpack Encore

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,13 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
-import './styles/app.css'
 
-// Bootstrap 5 imports
-import 'bootstrap/dist/css/bootsrap.min.css'
-import 'bootstrap'
-import '@popperjs/core'
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@popperjs/core';

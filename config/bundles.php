41c0f26c7c5c4320d45cf448f112e26cf71cda6e
corriol <vjorda@gmail.com>
2024-01-12 21:04:56 +0100

    addició de les llibreries per a pujada de fitxers

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];

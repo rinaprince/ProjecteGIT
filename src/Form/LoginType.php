4f0feb808931226a480e153c7bba87e83f79e5b7
nelopunxa <noahcalafatsom@gmail.com>
2024-01-26 08:11:11 +0100

    Modificacions en employee i EmployeeType, tambe en customercontroller i PrivateCustomerType, i les plantilles de employees, s'ha integrat lo de seguretat i rols a les pàgines respectives

---
+++ src/Form/LoginType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use App\Entity\Employee;
+use App\Entity\Login;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('password');
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Login::class,
+        ]);
+    }
+}

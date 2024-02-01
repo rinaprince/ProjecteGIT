c659959280dc608240a750dbec5809cd8a822d4b
Josep02 <josepferrer99@gmail.com>
2024-02-01 10:31:02 +0100

    New counter in the garage icon, new controller (ApiController)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\BrandRepository;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_front_office')]
-    public function index(BrandRepository $brandRepository): Response
+    public function index(BrandRepository $brandRepository, OrderRepository $orderRepository): Response
     {
-
         $brands = $brandRepository->findBy([], ['name' => 'ASC']);
 
         return $this->render('front_office/index_front.html.twig', [
-            'brands' => $brands,
+            'brands' => $brands
         ]);
     }
 

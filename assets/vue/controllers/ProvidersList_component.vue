5c8cde2bc293fea1b9ce1049313e696fbc465bf9
andreuador <andreusoriano2@gmail.com>
2024-02-01 09:59:20 +0100

    canvis en el controller de vehicle i afegir buscardors en el component de Provider

--- assets/vue/controllers/ProvidersList_component.vue
+++ assets/vue/controllers/ProvidersList_component.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="d-flex align-items-center">
-    <a :href="providerNewPath" class="m-2 btn btn-success">Create new</a>
+    <a :href="providerNewPath" class="m-2 btn btn-success">Crear</a>
     <form method="GET" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 mb-lg-1" role="search">
       <input name="q" type="search" class="form-control form-control-light text-dark"
              placeholder="Buscar..." aria-label="Search">
+    </form>
+    <form class="mb-3 mb-lg-0 me-lg-3 mb-lg-1">
+      <input v-model="filters.email.value" type="text" class="form-control" placeholder="Email...">
+    </form>
+    <form class="mb-3 mb-lg-0 me-lg-3 mb-lg-1">
+      <input v-model="filters.phone.value" type="text" class="form-control" placeholder="Mòbil..">
+    </form>
+    <form class="mb-3 mb-lg-0 me-lg-3 mb-lg-1">
+      <input v-model="filters.businessName.value" type="text" class="form-control" placeholder="Nom de l'empresa...">
     </form>
   </div>
   <table class="table">
@@ -23,7 +32,7 @@
     </tr>
     </thead>
     <tbody>
-    <tr v-for="provider in providers">
+    <tr v-for="provider in filteredProviders" :key="provider.id">
       <td>{{ provider.email }}</td>
       <td>{{ provider.phone }}</td>
       <td class="d-sm-none">{{ provider.dni }}</td>
@@ -35,10 +44,10 @@
       <td class="d-sm-none d-md-none">{{ provider.LOPDdocFile }}</td>
       <td class="d-sm-none d-md-none">{{ provider.constitutionArticle }}</td>
       <td>
-        <a @click="showModal(provider.id)" class="btn btn-outline-dark">show</a>
+        <a @click="showModal(provider.id)" class="btn btn-outline-dark">Mostrar</a>
       </td>
       <td>
-        <a :href="providerEditPath(provider.id)" class="btn btn-primary">edit</a>
+        <a :href="providerEditPath(provider.id)" class="btn btn-primary">Editar</a>
       </td>
     </tr>
     </tbody>
@@ -49,7 +58,8 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Provider</h5>
-          <button type="button" class="btn-close" @click="hideModal" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close" @click="hideModal" data-bs-dismiss="modal"
+                  aria-label="Close"></button>
         </div>
         <div class="modal-body">
 
@@ -58,7 +68,9 @@
           <div class="modal-confirmation"></div>
         </div>
         <div class="modal-footer">
-          <form method="post" action="{{ path('app_provider_delete', {'id': provider.id}) }}" onsubmit="return confirm('Are you sure you want to delete this item?');">
+          <!-- Fer que borre -->
+          <form method="post" action="{{ path('app_provider_delete', {'id': provider.id}) }}"
+                onsubmit="return confirm('Are you sure you want to delete this item?');">
             <input type="hidden" name="_token" value="{{ csrf_token('delete', provider.id) }}">
             <button class="btn btn-danger" id="deleteProvider">Delete</button>
           </form>
@@ -70,57 +82,57 @@
 
 <script setup>
 import axios from 'axios';
+import {ref, computed} from 'vue';
 
-defineProps({
-  providers: Array,
-  q: String
-});
+const props = defineProps(['providers', 'q']);
 
 const providerShowPath = (id) => `/providers/${id}`;
 const providerEditPath = (id) => `/providers/${id}/edit`;
 const providerNewPath = `/providers/new`;
 
+const filters = ref( {
+  global: { value: null, matchMode: 'CONSTRAINS' },
+  email: { value: null, matchMode: 'CONSTRAINS' },
+  phone: { value: null, matchMode: 'CONSTRAINS' },
+  businessName: { value: null, matchMode: 'CONSTRAINS' },
+});
 
-// ... (resto del código)
-    // Hacer la solicitud Axios aquí
-    function showModal(id) {
-      axios.get('/providers/'+id)
-          .then(response => {
-            // Actualizar el contenido del modal
-            const modalBody = document.querySelector('.modal-body');
-            modalBody.innerHTML = response.data;
+const filteredProviders = computed(() => {
+  return applyFilters(props.providers, filters.value);
+});
 
-            // Mostrar el modal
-            const myModal = document.querySelector('.modal');
-            myModal.style.display = 'block';
+const applyFilters = (data, filters) => {
+  return data.filter((provider) => {
+    return (
+        (!filters.global.value || JSON.stringify(provider).toLowerCase().includes(filters.global.value.toLowerCase())) &&
+        (!filters.email.value || provider.email.toLowerCase().includes(filters.email.value.toLowerCase())) &&
+        (!filters.phone.value || provider.phone.toLowerCase().includes(filters.phone.value.toLowerCase())) &&
+        (!filters.businessName.value || provider.businessName.toLowerCase().includes(filters.businessName.value.toLowerCase()))
+    );
+  });
+};
 
-            /**const confirmModal = document.querySelector('.modal-confirmation')
 
+// Hacer la solicitud Axios aquí
+function showModal(id) {
+  axios.get('/providers/' + id)
+      .then(response => {
+        // Actualizar el contenido del modal
+        const modalBody = document.querySelector('.modal-body');
+        modalBody.innerHTML = response.data;
 
-            const deleteButton = document.getElementById('#deleteProvider')
-            deleteButton.addEventListener('click',confirmationModal(id))
-                */
-          })
-          .catch(error => {
-            console.error('Error fetching modal content:', error);
-          });
-    }
-    function hideModal(){
-      const myModal = document.querySelector('.modal');
-      myModal.style.display = 'none';
-    }
+        // Mostrar el modal
+        const myModal = document.querySelector('.modal');
+        myModal.style.display = 'block';
 
-    /**
-    function confirmationModal(id){
-      axios.get('/providers/'+id)
-          .then(response => {
-            // Actualizar el contenido del modal
-            const confirmModal = document.querySelector('.modal-confirmation')
-            confirmModal.innerHTML = response.data;
+      })
+      .catch(error => {
+        console.error('Error fetching modal content:', error);
+      });
+}
 
-          })
-          .catch(error => {
-            console.error('Error fetching modal content:', error);
-          });
-    }*/
+function hideModal() {
+  const myModal = document.querySelector('.modal');
+  myModal.style.display = 'none';
+}
 </script>

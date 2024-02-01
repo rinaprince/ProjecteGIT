7693ea782336b2b106e1f48ac7cb17bc24d50971
Josep02 <josepferrer99@gmail.com>
2024-02-01 10:31:02 +0100

    New counter in the garage icon, new controller (ApiController)

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -8,6 +8,7 @@ use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;

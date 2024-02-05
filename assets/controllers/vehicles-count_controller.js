3a5bf7e5aa908d43679f220a4cadbf9f6018b7e7
Josep <98087218+Josep02@users.noreply.github.com>
2024-02-05 11:43:36 +0100

    Vehicles count controller

---
+++ assets/controllers/vehicles-count_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+export default class extends Controller {
+    static targets = [ "counter"]
+    connect(){
+        this.load();
+    }
+    async load() {
+        try {
+            const response = await fetch('/api/v1/count');
+            const data = await response.json();
+
+            if (data.count_number) {
+                this.counterTarget.textContent = data.count_number;
+            }
+        } catch (error) {
+            console.error('Fetch error:', error)
+        }
+    }
+}

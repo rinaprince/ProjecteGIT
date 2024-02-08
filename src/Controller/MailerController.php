8e8e8c76904c8dd4f2013a98b982db36506c4cc1
javieramosfuentes <jarafu04@gmail.com>
2024-02-08 11:34:15 +0100

    mailer done

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MailerController extends AbstractController
+{
+
+    #[Route('/email' ,name: 'app_send_email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('bhec@gmail.org')
+            ->to("exemple@gmail.com")
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('app_front_office');
+    }
+}

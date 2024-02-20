87ab105891b91f177d298ca1e38d13b6d5b8d13e
Josep02 <josepferrer99@gmail.com>
2024-02-20 13:52:15 +0100

    Process de venta implementat

---
+++ src/Form/PurchaseFormType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PurchaseFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('document', ChoiceType::class, [
+                'choices' => [
+                    'DNI' => 'DNI',
+                    'NIE' => 'NIE',
+                    'Passaport' => 'Passaport',
+                ],
+                'label' => 'Documentació'
+            ])
+            ->add('documentNumber', TextType::class, [
+                'label' => 'Número de document'
+            ])
+            ->add('emailInput', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('phoneInput', TelType::class, [
+                'label' => 'Mòbil'
+            ])
+            ->add('paymentMethod', ChoiceType::class, [
+                'choices' => [
+                    'PayPal' => 'PayPal',
+                    'Visa' => 'Visa',
+                    'MasterCard' => 'MasterCard',
+                ],
+                'label' => 'Mètode de pagament'
+            ])
+            ->add('nameInput', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('lastNameInput', TextType::class, [
+                'label' => 'Cognoms'
+            ])
+            ->add('termsCheckbox', CheckboxType::class, [
+                'label' => 'Acceptar termes i condicions d\'ús.',
+                'required' => false
+            ])
+            ->add('addressName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('addressLastName', TextType::class, [
+                'label' => 'Cognoms'
+            ])
+            ->add('provinceAddress', TextType::class, [
+                'label' => 'Provincia'
+            ])
+            ->add('postalCode', TextType::class, [
+                'label' => 'Codi postal'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ciutat'
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Direcció'
+            ])
+            ->add('details', TextType::class, [
+                'label' => 'Pis i porta'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
